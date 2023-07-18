740e86c33e30adfa50f3e7e4befe8e05b43c94b3
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-07-18 17:30:23 +0700

    restructure repo

--- FrontendPro/pricing-card-page/src/components/Card.styled.tsx
+++ FrontendPro/pricing-card-page/src/components/Card.styled.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
 
+// CardContainer
 export const CardContainer = styled.div({
 
 })
 
+// CardTitle
+export const CardTitle = styled.h1({
+})
+
+// CardContent
 export const CardContent = styled.div({})
